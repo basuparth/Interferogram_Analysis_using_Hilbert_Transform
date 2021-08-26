b07538b3ca9bf42f9fc2c0c32cc2651cb3a31f7a
basuparth <47746249+basuparth@users.noreply.github.com>
2021-08-26 22:44:34 +0530

    Added code for calibrating the CCD camera

---
+++ Calibrating_the_CCD_camera.m
@@ -0,0 +1,13 @@
+pixel=[524 608 644];
+wavel=[508.6 480 467.8];
+p=polyfit(pixel,wavel,1);
+x=100:1:724;
+y=p(1)*x+p(2);
+plot(x,y,pixel,wavel,'*')
+hold on
+plot(124,643.8,'*m')
+hold off
+title('pixel vs wavelength')
+xlabel('pixel')
+ylabel('wavelength in nm')
+grid on 
